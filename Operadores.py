7adae5ffe67e7d2e1b3a2ee8ebe48a453febe544
Juan Felipe <juanfelipe202322@gmail.com>
2024-04-13 01:49:41 -0300

    commit

---
+++ Operadores.py
@@ -0,0 +1,32 @@
+x = 5
+y = 10
+
+# Verifica se x é igual a 5
+if x == 5:
+    print("x é igual a 5")
+
+# Verifica se x é maior que y
+if x > y:
+    print("x é maior que y")
+else:
+    print("x não é maior que y")
+
+# Verifica se x é menor que y
+if x < y:
+    print("x é menor que y")
+else:
+    print("x não é menor que y")
+
+# Verifica se x é maior ou igual a 5
+if x >= 5:
+    print("x é maior ou igual a 5")
+
+# Verifica se x é menor ou igual a 5
+if x <= 5:
+    print("x é menor ou igual a 5")
+
+# Verifica se x é diferente de y
+if x != y:
+    print("x é diferente de y")
+else:
+    print("x é igual a y")
